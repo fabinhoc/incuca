a87b6649b1e705f3ad6d3b90475205f15185ca32
Fabio Cruz <fabio.cruz@etus.group>
2020-09-25 20:10:43 -0300

    Change nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -54,7 +54,7 @@ export default {
   },
 
   proxy: {
-    '/joke/': { target: 'http://geek-jokes.sameerkumar.website/api?format=json', pathRewrite: { '^/joke/': '' } }
+    '/joke/': { target: 'https://geek-jokes.sameerkumar.website/api?format=json', pathRewrite: { '^/joke/': '' } }
   },
 
   // Vuetify module configuration (https://go.nuxtjs.dev/config-vuetify)
